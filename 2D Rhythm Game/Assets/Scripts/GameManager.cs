c9d05ae6abb04e58c46272d9e83e60de4378c87f
dnfqh0905 <54350225+dnfqh0905@users.noreply.github.com>
2019-08-30 12:40:24 +0900

    구분선 추가, 노트 오브젝트 생성 및 이동

---
+++ 2D Rhythm Game/Assets/Scripts/GameManager.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    // 싱글톤 처리
+    public static GameManager instance { get; set; }
+    private void Awake()
+    {
+        if (instance == null)
+            instance = this;
+        else if (instance != this)
+            Destroy(gameObject);
+    }
+    public float noteSpeed;
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
