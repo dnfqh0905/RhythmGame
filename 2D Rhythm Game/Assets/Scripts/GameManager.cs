4d71f9a5d796673d5aab689dd6c0eaf2200e0fb3
dnfqh0905 <54350225+dnfqh0905@users.noreply.github.com>
2019-08-30 13:49:15 +0900

    충돌 감지 및 노트 판정 이벤트

--- 2D Rhythm Game/Assets/Scripts/GameManager.cs
+++ 2D Rhythm Game/Assets/Scripts/GameManager.cs
@@ -15,14 +15,23 @@ public class GameManager : MonoBehaviour
     }
     public float noteSpeed;
 
+    /*
+     * BAD : 1
+     * GOOD : 2
+     * PERFECT : 3
+     * MISS : 4
+     */
+
+    public enum judges { NONE = 0, BAD, GOOD, PERFECT, MISS };
+
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
 }
