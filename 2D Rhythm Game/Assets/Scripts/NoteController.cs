f2eb112e2f66b3ccf136f344eb68ddcd45fee8bc
dnfqh0905 <54350225+dnfqh0905@users.noreply.github.com>
2019-09-02 15:11:01 +0900

    순위 판정 점수 데이터 삽입

--- 2D Rhythm Game/Assets/Scripts/NoteController.cs
+++ 2D Rhythm Game/Assets/Scripts/NoteController.cs
@@ -4,6 +4,9 @@ using System.Collections.Generic;
 using System.IO;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using Firebase;
+using Firebase.Database;
+using Firebase.Unity.Editor;
 
 public class NoteController : MonoBehaviour
 {
@@ -101,10 +104,43 @@ public class NoteController : MonoBehaviour
         PlayerInformation.score = GameManager.instance.score;
         PlayerInformation.musicTitle = musicTitle;
         PlayerInformation.musicArtist = musicArtist;
+        AddRank();
         SceneManager.LoadScene("GameResultScene");
     }
 
-    // Update is called once per frame
+    // 순위 정보를 담는 Rank 클래스 정의
+    class Rank
+    {
+        public string email;
+        public int score;
+        public double timestamp;
+
+        public Rank(string emal, int score, double timestamp)
+        {
+            this.email = email;
+            this.score = score;
+            this.timestamp = timestamp;
+
+        }
+    }
+
+    void AddRank()
+    {
+        // 데이터베이스 접속 설정
+        DatabaseReference reference;
+        FirebaseApp.DefaultInstance.SetEditorDatabaseUrl("https://unity-rhythm-game-tutori-1f7bf.firebaseio.com/");
+        reference = FirebaseDatabase.DefaultInstance.RootReference;
+
+        // 삽입할 데이터 준비
+        DateTime now = DateTime.Now.ToLocalTime();
+        TimeSpan span = (now - new DateTime(1970, 1, 1, 0, 0,0).ToLocalTime());
+        int timestamp = (int)span.TotalSeconds;
+        Rank rank = new Rank(PlayerInformation.auth.CurrentUser.Email, (int)PlayerInformation.score, timestamp);
+        string json = JsonUtility.ToJson(rank);
+        // 랭킹 점수 데이터 삽입
+        reference.Child("ranks").Child(PlayerInformation.selectedMusic).Child(PlayerInformation.auth.CurrentUser.UserId).SetRawJsonValueAsync(json);
+    }
+
     void Update()
     {
 
